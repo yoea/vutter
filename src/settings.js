5a118cc56cff1dc66c92fa43f14a8a16bf9b5d7b
Kaelvean <yyy_ag@163.com>
2023-03-20 00:06:21 +0800

    初始化提交vutter

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '尤恩的管理后台',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
---
+++ src/store/modules/user.js
@@ -0,0 +1,116 @@
+import { login, logout, getInfo } from '@/api/user'
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { resetRouter } from '@/router'
+
+const getDefaultState = () => {
+  return {
+    token: getToken(),
+    name: '',
+    avatar: '',
+    nickname: '',
+    email: '',
+    motto: ''
+  }
+}
+
+const state = getDefaultState()
+
+const mutations = {
+  RESET_STATE: (state) => {
+    Object.assign(state, getDefaultState())
+  },
+  SET_TOKEN: (state, token) => {
+    state.token = token
+  },
+  SET_AVATAR: (state, avatar) => {
+    state.avatar = avatar
+  },
+  SET_NAME: (state, name) => {
+    state.name = name
+  },
+  SET_EMAIL: (state, email) => {
+    state.email = email
+  },
+  SET_NICKNAME: (state, nickname) => {
+    state.nickname = nickname
+  },
+  SET_MOTTO: (state, motto) => {
+    state.motto = motto
+  }
+}
+
+const actions = {
+  // user login
+  login({ commit }, userInfo) {
+    const { username, password } = userInfo
+    return new Promise((resolve, reject) => {
+      login({ username: username.trim(), password: password }).then(response => {
+        console.log(response)
+        const { data } = response
+        commit('SET_TOKEN', data.token)
+        setToken(data.token)
+        resolve()
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
+
+  // get user info
+  getInfo({ commit, state }) {
+    return new Promise((resolve, reject) => {
+      console.log('准备getInfo')
+      getInfo(state.token).then(response => {
+        const { data } = response
+
+        if (!data) {
+          reject('Verification failed, please Login again.')
+        }
+
+        const { username, avatar, email, nickname, motto } = data
+
+        commit('SET_NAME', username)
+        commit('SET_AVATAR', avatar)
+        commit('SET_EMAIL', email)
+        commit('SET_NICKNAME', nickname)
+        commit('SET_MOTTO', motto)
+
+        resolve(data)
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
+
+  // user logout
+  logout({ commit, state }) {
+    return new Promise((resolve, reject) => {
+      logout({ 'token': state.token }).then(res => {
+        console.log(res)
+        removeToken() // must remove  token  first
+        resetRouter()
+        commit('RESET_STATE')
+        resolve()
+      }).catch(error => {
+        reject(error)
+      })
+    })
+  },
+
+  // remove token
+  resetToken({ commit }) {
+    return new Promise(resolve => {
+      removeToken() // must remove  token  first
+      commit('RESET_STATE')
+      resolve()
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
+
